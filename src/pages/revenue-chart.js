f91587f5f15af9d60f1837eb2f34f39f9ef4098b
Akinoobi <gelo.palma23@gmail.com>
2023-01-30 17:02:08 +0800

    chore: display and navbar
    Signed-off-by: Akinoobi <gelo.palma23@gmail.com>

---
+++ src/pages/revenue-chart.js
@@ -0,0 +1,11 @@
+
+import AppBar from '@mui/material/AppBar';
+import { DefaultLayout } from '../component/DefaultLayout';
+// or
+
+export default function Index ({props}) {
+
+    return (
+        <DefaultLayout> test </DefaultLayout>
+    )
+}
